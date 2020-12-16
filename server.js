64634b47e0cf16d3aeeb46b47e8a0da35d69908a
Victor Tran <victortran415@gmail.com>
2020-12-16 11:21:01 -0800

    created htmlRoutes.js file

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const PORT = 5000;
 
 const apiRoutes = require("./routes/apiRoutes")
+const htmlRoutes = require("./routes/htmlRoutes")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/api", apiRoutes)
+app.use("/", htmlRoutes)
 
 app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`))
