d6d4e793f8439ba329547a072ff1f03a254e7c92
Victor Tran <victortran415@gmail.com>
2020-12-16 11:16:15 -0800

    added get and post to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
 const fs = require("fs");
+const notesDB = require("../db/db.json")
 
 
+router.get("/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+})
+
+router.post("/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+
+        notes.push(req.body)
+        console.log(notes)
+
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        if (err) return res.JSON({ err: "trouble adding notes" });
+        res.json({ msg: "Notes added" })
+        });
+    });
+})
 
 
 
