7cab38734fd7137cbd9736b38b998aa3a2f1cc38
Victor Tran <victortran415@gmail.com>
2020-12-18 20:26:35 -0800

    was able to add text, and save

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ const notesDB = require("../db/db.json")
 
 
 router.get("/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
     });
@@ -12,24 +12,23 @@ router.get("/notes", (req, res) => {
 
 router.post("/notes", (req, res) => {
     console.log(req.body)
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
 
-        notes.push(
-            {
-                title: req.body.title,
-                text: req.body.text,
-            }
-        );
-        console.log(notes)
+        notes.push({
+            title: req.body.title,
+            text: req.body.text
+        });
 
-        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
-        if (err) return res.JSON({ err: "trouble adding notes" });
-        res.json({ msg: "Notes added" })
+        fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
+            if (err) return res.JSON({ err: "Adding Unsuccessful"});
+            res.json ({msg: "Added Successfully"})
         });
     });
 })
+
+
 
 
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const path= require ("path");
+const path = require ("path");
 
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../notes.html"));
@@ -11,9 +11,9 @@ router.get("/", (req, res) => {
 
 router.get("/assets/css/styles.css", (req,res) => {
     res.sendFile(path.join(__dirname, "../assets/css/styles.css")); 
- })
+})
 
- router.get("/js/index.js", (req,res) => {
+router.get("/assets/js/index.js", (req,res) => {
     res.sendFile(path.join(__dirname, "../assets/js/index.js")); 
- })
+})
 module.exports = router
